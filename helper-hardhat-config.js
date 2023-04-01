2e1f692302b7dbd3e9320002a5ff78abdd142a33
kangaroo-data-dao <prasadax.g@gmail.com>
2023-04-01 18:02:15 +0530

    nu

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     3141: {
         name: "hyperspace",
-        tokensToBeMinted: 12000,
+        tokensToBeMinted: 120000,
     },
 }
 
